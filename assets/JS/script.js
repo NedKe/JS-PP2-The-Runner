37bdb62f8c574dc0cd06d2e2366d4b034af568ef
Neda <neda@Nedas-MacBook-Pro.local>
2021-10-01 14:56:38 +0400

    Add icon to toss function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -73,14 +73,9 @@ const increases = {
 let currentPosition = 0;
 const user = document.querySelector('#game #user');
 const blocks = document.querySelectorAll('#game > div');
-// this function creates the toss numbers //
-
 let dice = document.getElementById("dice");
+const icon = document.querySelector('#dice i');
 
-function toss() {
-    dice.innerHTML = Math.floor(Math.random() * 6) + 1;
-
-}
 for (let i in increases) {
     blocks[i].innerHTML = increases[i].value;
     if (increases[i].type === 'green') {
@@ -96,3 +91,42 @@ if (randomValue === 2) {
     currentPosition += 5;
     blocks[currentPosition].appendChild(user);
 }
+
+function toss() {
+    return 1 + Math.floor(Math.random() * 6);
+}
+/* 
+font-awesome classes are like fa-dice-one, fa-dice-two
+This is a helper function to convert 1 to one and 2 to two etc
+*/
+function convertDigitToCharacter(digit) {
+    switch (digit) {
+        case 1:
+            return 'one';
+        case 2:
+            return 'two';
+        case 3:
+            return 'three';
+        case 4:
+            return 'four';
+        case 5:
+            return 'five';
+        case 6:
+            return 'six';
+    }
+}
+// we always hold the value of the current icon class so that we remove it when we roll the dice and add the new value. Initially it is fa-dice as in html
+let currentDiceIcon = 'fa-dice';
+
+// event listener to handle the click
+dice.addEventListener('click', function () {
+    const randomValue = toss();
+    console.log(randomValue)
+    // remove the current icon's dice class    
+    icon.classList.remove(currentDiceIcon);
+    // update the current dice icon with the new value based on toss and convert it to the fa dice class respective to the number
+    currentDiceIcon = `fa-dice-${convertDigitToCharacter(randomValue)}`;
+
+    // add the new dice class to the icon
+    icon.classList.add(currentDiceIcon);
+})
