ac7fc4bd30f477540889caea35b976436f58194c
Neda <neda@Nedas-MacBook-Pro.local>
2021-10-07 12:33:22 +0400

    Add levels , responsive for 500px, questionsanswers and increases

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -10,8 +10,58 @@ let StarGamePopup = document.getElementsByClassName('popup-gameStart')[0];
 let EndGamePopup = document.getElementsByClassName('popup-gameEnd')[0];
 const myBackgroundMusic = new sound("assets/music/background.mp3", true);
 let muteBtn = document.querySelector("#mute");
+let resultMsg = document.querySelector("#result");
 let backgroundMusicOn = true;
+let levelPopup = document.querySelector("#choose-level");
+let levelBasicBtn = document.querySelector("#choose-level #basic");
+let levelHardBtn = document.querySelector("#choose-level #hard");
+let level = 1;
+levelBasicBtn.addEventListener('click', function () {
+    countDown();
+    levelPopup.classList.remove("show");
+    level = 1;
+    createCheckedPattern();
+    drawIncreasesAndQuestions()
+})
+levelHardBtn.addEventListener('click', function () {
+    countDown();
+    levelPopup.classList.remove("show");
+    level = 2;
+    createCheckedPattern();
+    drawIncreasesAndQuestions()
+})
 
+function drawIncreasesAndQuestions() {
+    drawIncreases();
+    drawQuestions();
+}
+
+
+let gameTime;
+let timer = document.querySelector("#stop-watch")
+
+
+function resetTimer() {
+    gameTime = 4;
+    timer.innerHTML = gameTime + "";
+}
+
+resetTimer();
+
+let timerId;
+
+function countDown() {
+    timerId = setInterval(function () {
+        gameTime = gameTime - 1;
+        timer.innerHTML = gameTime + "";
+        if (gameTime === 0) {
+            if (currentPosition < 99) {
+                showEndPopup("You lost!");
+            }
+            clearInterval(timerId);
+        }
+    }, 1000)
+}
 
 function move(randomNumber) {
     currentPosition += randomNumber;
@@ -30,26 +80,39 @@ function move(randomNumber) {
     }
     if (currentPosition >= 99) {
         currentPosition = 99;
-        showEndPopup();
+        clearInterval(timerId);
+        showEndPopup("Well done!");
     }
     blocks[currentPosition].appendChild(user);
 
 }
 
 function drawIncreases() {
-    for (let key in increases) {
-        blocks[key].style.backgroundColor = increases[key].type;
-        blocks[key].innerHTML = increases[key].value;
+    let rewardPunishment;
+    if (level === 1) {
+        rewardPunishment = increases;
+    } else if (level === 2) {
+        rewardPunishment = increasesLevel2;
+    }
+    for (let key in rewardPunishment) {
+        blocks[key].style.backgroundColor = rewardPunishment[key].type;
+        blocks[key].innerHTML = rewardPunishment[key].value;
     }
 }
-drawIncreases();
+
 
 function drawQuestions() {
-    for (let key in questions) {
+    let currentQuestions;
+    if (level === 1) {
+        currentQuestions = questions;
+    } else if (level === 2) {
+        currentQuestions = questionsLevel2;
+    }
+    for (let key in currentQuestions) {
         blocks[key].innerHTML = '?'
     }
 }
-drawQuestions();
+
 
 function showPopup() {
     questionAnswersPopup.classList.add("visible")
@@ -70,7 +133,7 @@ function createOptions(answers) {
     }
 }
 submitBtn.addEventListener('click', function () {
-    let userAnswer = parseInt(document.querySelector("[name='answer']:checked").value);
+    let userAnswer = document.querySelector("[name='answer']:checked").value;
     if (userAnswer === questions[currentPosition].rightAnswer) {
         move(2)
     } else {
@@ -97,6 +160,8 @@ function sound(src, loop) {
 startBtn.addEventListener('click', function () {
     hideStartPopup();
     myBackgroundMusic.play();
+    levelPopup.classList.add("show")
+    // countDown();
 })
 muteBtn.addEventListener('click', function () {
     if (backgroundMusicOn) {
@@ -113,7 +178,8 @@ function hideStartPopup() {
 }
 
 
-function showEndPopup() {
+function showEndPopup(message) {
+    resultMsg.innerHTML = message;
     EndGamePopup.style.visibility = "visible";
 }
 
@@ -123,18 +189,25 @@ function hideEndPopup() {
 restartBtn.addEventListener('click', function () {
     hideEndPopup();
     currentPosition = 0;
+    resetTimer();
     blocks[currentPosition].appendChild(user);
-})
+    levelPopup.classList.add("show");
+
+});
+
 
 function createCheckedPattern() {
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
+            if (i !== 0 && j !== 0) {
+                blocks[10 * i + j].innerHTML = "";
+            }
             if (j % 2 === (i % 2 === 0 ? 0 : 1)) {
                 blocks[10 * i + j].style.backgroundColor = "#cbeaf1"
+            } else {
+                blocks[10 * i + j].style.backgroundColor = "#fff"
             }
         }
-
-
     }
 }
 createCheckedPattern();
--- assets/JS/quiz.js
+++ assets/JS/quiz.js
@@ -1,14 +1,34 @@
 const increases = {
     3: {
-        value: 2,
+        value: 8,
         type: "green",
     },
     8: {
-        value: -6,
+        value: -3,
         type: "red",
+    },
+    22: {
+        value: 8,
+        type: "red",
+    },
+    35: {
+        value: 8,
+        type: "green",
+    },
+    39: {
+        value: 8,
+        type: "green",
     },
     53: {
         value: 3,
+        type: "red",
+    },
+    67: {
+        value: 3,
+        type: "green",
+    },
+    71: {
+        value: 8,
         type: "green",
     },
     85: {
@@ -16,7 +36,91 @@ const increases = {
         type: "red",
     }
 }
+const increasesLevel2 = {
+    12: {
+        value: 8,
+        type: "green",
+    },
+    43: {
+        value: -6,
+        type: "red",
+    }
+}
 const questions = {
+    4: {
+        question: "What is the capital of Georgia?",
+        multipleAnswers: ["Tokyo", "Istanbul", "Tbilisi", "Washington"],
+        rightAnswer: "Tbilisi",
+    },
+    11: {
+        question: "What is the name of the first decentralized cryptocurrency",
+        multipleAnswers: ["Ethereum", "Bitcoin", "Ripple", "Litecoin"],
+        rightAnswer: "Bitcoin",
+    },
+    17: {
+        question: "Where was pizza invented",
+        multipleAnswers: ["Spain", "Greece", "Italy", "Germany"],
+        rightAnswer: 9,
+    },
+    24: {
+        question: "Which book belongs to George Orwell?",
+        multipleAnswers: ["Jane Eyre", "Crime and Punishment", "1984", "Wuthering Heights"],
+        rightAnswer: 9,
+    },
+    29: {
+        question: "What the capital of Eduador?",
+        multipleAnswers: ["Santo Domingo", "Quito", "San Jose", "Brasilia"],
+        rightAnswer: 9,
+    },
+    32: {
+        question: "Which composer composed Für Elise",
+        multipleAnswers: ["Bach", "Chopin", "Mozart", "Beethoven"],
+        rightAnswer: "Beethoven",
+    },
+    37: {
+        question: "What is the real name of Lady Gaga?",
+        multipleAnswers: ["Stefani", "Sara", "Isabella", "Sophia"],
+        rightAnswer: "Stefani",
+    },
+
+    45: {
+        question: "What does Dozen mean?",
+        multipleAnswers: [6, 18, 12, 24],
+        rightAnswer: 12,
+    },
+    59: {
+        question: "The largest ocean in the world is?",
+        multipleAnswers: ["Atlantic Ocean", "Indian Ocean", "Arctic ocean", "Pacific ocean"],
+        rightAnswer: "Pacific ocean",
+    },
+    61: {
+        question: "What instrument is used to look at the stars?",
+        multipleAnswers: ["Telescope", "Microscope", "Camera", "Glasses"],
+        rightAnswer: "Telescope",
+    },
+    66: {
+        question: "What is the language spoken in Hong Kong?",
+        multipleAnswers: ["Cantonese", "Mandarin", "Cebuano", "Malay"],
+        rightAnswer: "Cantonese",
+    },
+    73: {
+        question: "What is the fastest car with speed of 316 mph in 2021",
+        multipleAnswers: ["Tuatara", "Koenigsegg", "Bugatti", "Ferarri"],
+        rightAnswer: "Tuatara",
+    },
+    78: {
+        question: "What is the capital of Germany?",
+        multipleAnswers: ["Bern", "Munich", "Berlin", "Frankfurt"],
+        rightAnswer: "Berlin",
+    },
+    80: {
+        question: "Which country give the nobel prize in natural sciences?",
+        multipleAnswers: ["Switzerland", "Norway", "Finland", "Sweden"],
+        rightAnswer: "Sweden",
+    },
+}
+
+const questionsLevel2 = {
     4: {
         question: "What is the sum of 4 and 5?",
         multipleAnswers: [6, 3, 1, 9],
