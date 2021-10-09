95a8620b73461c62a6fffd5acd7e62e133326ddd
Neda <neda.keshavarz81@gmail.com>
2021-10-09 22:12:59 +0400

    Move toss functionality into game.js

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -14,13 +14,21 @@ const levelPopup = document.querySelector("#choose-level");
 const levelBasicBtn = document.querySelector("#choose-level #basic");
 const levelHardBtn = document.querySelector("#choose-level #hard");
 const timer = document.querySelector("#stop-watch");
-
+const icon = document.querySelector('#dice i');
 let gameTime;
 let timerId;
 let backgroundMusicOn = true;
 let currentPosition = 0;
 let tempPosition;
 let level = 1;
+let currentDiceIcon = 'fa-dice';
+
+
+/* Storing the value of the current icon class , fa-dice, so that we remove it when 
+we roll the dice and add the new value.*/
+function toss() {
+    return 1 + Math.floor(Math.random() * 6);
+}
 
 function initializeLevel(chosenLevel) {
     level = chosenLevel;
@@ -32,6 +40,19 @@ function initializeLevel(chosenLevel) {
 }
 
 function initializeEventListeners() {
+    // Event listener on the dice icon to handle the click
+    dice.addEventListener('click', function () {
+        const randomValue = toss();
+        console.log(randomValue)
+        // remove the current icon's dice class    
+        icon.classList.remove(currentDiceIcon);
+        // update the current dice icon with the new value based on toss and convert it to the fa dice class respective to the number
+        currentDiceIcon = `fa-dice-${convertDigitToCharacter(randomValue)}`;
+
+        // add the new dice class to the icon
+        icon.classList.add(currentDiceIcon);
+        move(randomValue);
+    })
     //Event listener for buttons basic and hard
     levelBasicBtn.addEventListener('click', function () {
         initializeLevel(1);
